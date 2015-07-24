142386a12b93ac7c64f2797de7690814bb2814fc
AaronHagenson <thedestroyer9876@yahoo.com>
2015-07-24 23:53:28 +0000

    add image upload for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,22 @@ class User < ActiveRecord::Base
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :picture, UserPictureUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts       
   has_many :forum_comments
   has_many :forum_threads
+  validate :picture_size
+  
+  private
+  
+  # Validates the size of an uploaded picture.
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
+
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724203603) do
+ActiveRecord::Schema.define(version: 20150724225510) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20150724203603) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
