29650627fbff2ff42968fe82145924ac3e4d0361
AaronHagenson <thedestroyer9876@yahoo.com>
2015-08-18 18:50:56 +0000

    fix database errors

--- db/migrate/20150814000856_change_orientation_datatype.rb
+++
@@ -1,5 +0,0 @@
-class ChangeOrientationDatatype < ActiveRecord::Migration
-  def change
-     change_column :posts, :image_orientation, :string
-  end
-end
--- db/migrate/20150817210544_redo_orientation_datatype.rb
+++
@@ -1,5 +0,0 @@
-class RedoOrientationDatatype < ActiveRecord::Migration
-  def change
-    change_column :posts, :image_orientation, :integer
-  end
-end
